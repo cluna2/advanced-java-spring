75d869c233f5d8a7ac6b818932ac526e427ea95b
cluna2 <lunacris2739@gmail.com>
2025-09-02 16:09:27 -0700

    Response entity examples

--- src/main/java/com/codingnomads/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
+++ src/main/java/com/codingnomads/springweb/returningdatatoclient/usingresponseentity/controller/ResponseController.java
@@ -3,17 +3,22 @@ package com.codingnomads.springweb.returningdatatoclient.usingresponseentity.con
 
 import com.codingnomads.springweb.returningdatatoclient.usingresponseentity.model.User;
 import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ResponseController {
 
     User user = new User(1, "Test User", "test@email.com");
+
+    Map<Integer, User> userMap = new HashMap<>(Map.of(1, user));
 
     @GetMapping("/constructor")
     public ResponseEntity<User> constructorMethod() {
@@ -36,6 +41,30 @@ public class ResponseController {
             return new ResponseEntity<>(user, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping(value = "/practice", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> practice(@RequestBody User user) throws URISyntaxException {
+        if (user.getId() < 0) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .header("TEST", "TEST HEADER")
+                    .body(null);
+        }
+        userMap.put(user.getId(), user);
+        return ResponseEntity
+                .created(new URI("/practice" + user.getId()))
+                .header("TEST", "TEST HEADER")
+                .body(user);
+    }
+
+    @GetMapping("/practice/{id}")
+    public ResponseEntity<?> practiceGet(@PathVariable int id) {
+        if (userMap.containsKey(id)) {
+            return ResponseEntity.ok().body(userMap.get(id));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
 }
